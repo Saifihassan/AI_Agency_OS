fc62de8becc28b7bea0fff44fac20cd856188599
Saifihassan <saifihassan656@gmail.com>
2026-07-08 11:56:39 +0530

    initial commit of agency os

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AI Agency OS",
+    page_icon="🤖",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+with st.sidebar():
+    st.write()
+
